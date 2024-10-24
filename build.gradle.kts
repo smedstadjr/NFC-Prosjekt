0c4f18a1ad58a87b9f735a66455edd00ae7cb8de
smedstadjr <sandersm@hiof.no>
2024-10-24 20:42:12 +0200

    Update build.gradle.kts
    adding dependencies for roboelectronic

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
+    testImplementation(libs.robolectric)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
